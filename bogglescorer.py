528c6fa4d08058efa923634b51bea74ced0af4fa
Simon Murphy <simonmurphy@MacBook-Pro.local>
2020-11-13 22:43:51 +0000

    check_words implemented

--- bogglescorer.py
+++ bogglescorer.py
@@ -2,11 +2,19 @@
 """This script checks a list of words against the UK Boggle dictionary and returns
 the valid words and scores"""
 
+with open('sowpods.txt', 'r') as f:
+    sowpods = f.readlines()
+valid_words = [x.strip() for x in sowpods]
+
 def check_words(words):
     """
     returns a list of words checked against the UK Boggle dictionary.
     """
-    return words
+    checked_words = []
+    for word in words:
+        if word.upper() in valid_words:
+            checked_words.append(word)
+    return checked_words
 
 def score_words(words):
     """
@@ -22,9 +30,12 @@ def score_words(words):
     return scored
 
 if __name__ == "__main__":
-    examples = ['hello', 'hat', 'chase', 'hats', '']
-    scored_list = score_words(examples)
+    examples = ['hello', 'hat', 'chase', 'catte', 'hats', '', 'fhir']
+    print(examples)
+    print(check_words(examples))
+    scored_list = score_words(check_words(examples))
     example_words, example_scores = zip(*scored_list)
     print(example_words, example_scores)
     print('Total Score: ', sum(example_scores))
     print('Number of words: ', len(example_words))
+    print('Total valid words:', len(valid_words))
