4da55bf934e7055649d05a074da76e225ec620ac
smurphboy <simon.murphy@gmail.com>
2020-11-15 20:54:10 +0000

    Square logic updated

--- boggleui.py
+++ boggleui.py
@@ -27,8 +27,14 @@ def clicked(data, col, row, num):
     disabled.append(num)
     word.append(data)
     print(data, col, row, 'word: ', word, 'num: ', num, 'disabled: ', disabled)
+    for square in board_squares:
+        square.disable()
+    print(CONNECTED[num])
+    for element in CONNECTED[num]:
+        board_squares[element].enable()
     for element in disabled:
         board_squares[element].disable()
+    current_word.value = (''.join(word))
 
 
 def submit_word():
@@ -36,19 +42,32 @@ def submit_word():
     Clears the grid, enables all the dice, checks and scores the word and adds to the word list
     """
     print(''.join(word))
-    for element in disabled:
-        board_squares[element].enable()
+    for square in board_squares:
+        square.enable()
     wordlist.append(''.join(word))
     wordlist.append('\n')
     disabled.clear()
     word.clear()
+    current_word.value = ''
     # score word, add to list
 
 def clear_word():
     """
     Clears the grid, enables all the dice and clears the word
     """
+    for square in board_squares:
+        square.enable()
+    disabled.clear()
+    word.clear()
+    current_word.value = ''
 
+
+CONNECTED = [[1, 4, 5], [0, 2, 4, 5, 6], [1, 3, 5, 6, 7], [2, 6, 7],
+             [0, 1, 5, 8, 9], [0, 1, 2, 4, 6, 8, 9, 10],
+             [1, 2, 3, 5, 7, 9, 10, 11], [2, 3, 6, 10, 11],
+             [4, 5, 9, 12, 13], [4, 5, 6, 8, 10, 12, 13, 14],
+             [5, 6, 7, 9, 11, 13, 14, 15], [6, 7, 10, 14, 15],
+             [8, 9, 13], [8, 9, 10, 12, 14], [9, 10, 11, 13, 15], [10, 11, 14]]
 app = App("Boggle Scorer", layout="grid")
 top_box = Box(app, layout='grid', border=1, grid=[0, 0])
 rest = Box(app, layout='grid', border=1, grid=[0, 1])
@@ -66,6 +85,8 @@ word_list = Box(rest, layout='grid', grid=[1, 1], border=2, align='top')
 word_box = Box(rest, layout='grid', grid=[0, 2])
 wordtitle = Text(word_list, text='Word List', grid=[0, 0])
 wordlist = Text(word_list, text='', grid=[0, 1])
+current_word_title = Text(word_list, text='Current Word:', grid=[0, 2], align='bottom')
+current_word = Text(word_list, text=(''.join(word)), grid=[0, 3])
 submit = PushButton(word_box, text='Submit', command=submit_word, grid=[0, 0])
 clear = PushButton(word_box, text='Clear', command=clear_word, grid=[1, 0])
 app.display()
