a68931fafeb6a35e0f2a01f6c3af952468c719e7
Simon Murphy <simonmurphy@MacBook-Pro.local>
2020-11-15 21:28:43 +0000

    Linted in flake8

--- boggleui.py
+++ boggleui.py
@@ -3,6 +3,7 @@
 
 from guizero import App, PushButton, Box, Text
 
+
 def create_board(hand):
     """ Creates the board for the app. Takes a set of 16 dice """
     row = 0
@@ -14,15 +15,16 @@ def create_board(hand):
                                grid=[col, row], width=2, height=1))
         col = col+1
         if col == 4:
-            row = row +1
+            row = row + 1
             col = 0
     return dice
 
 
 def clicked(data, col, row, num):
     """
-    disable the clicked button and all buttons that aren't around the clicked letter
-    and haven't been selected already, return the letter to add to the selected word.
+    disable the clicked button and all buttons that aren't around the
+    clicked letter and haven't been selected already, return the letter
+    to add to the selected word.
     """
     disabled.append(num)
     word.append(data)
@@ -39,7 +41,8 @@ def clicked(data, col, row, num):
 
 def submit_word():
     """
-    Clears the grid, enables all the dice, checks and scores the word and adds to the word list
+    Clears the grid, enables all the dice, checks and scores the word and
+    adds it to the word list
     """
     print(''.join(word))
     for square in board_squares:
@@ -50,6 +53,7 @@ def submit_word():
     word.clear()
     current_word.value = ''
     # score word, add to list
+
 
 def clear_word():
     """
@@ -72,7 +76,8 @@ app = App("Boggle Scorer", layout="grid")
 top_box = Box(app, layout='grid', border=1, grid=[0, 0])
 rest = Box(app, layout='grid', border=1, grid=[0, 1])
 title = Text(top_box, text='Time: ', grid=[0, 0], align='left', width='fill')
-title2 = Text(top_box, text='Score: ', grid=[1, 0], align='right', width='fill')
+title2 = Text(top_box, text='Score: ', grid=[1, 0],
+              align='right', width='fill')
 dice_box = Box(rest, layout='grid', grid=[0, 1], border=2)
 test_hand = ['A', 'B', 'C', 'D',
              'E', 'F', 'G', 'H',
@@ -85,7 +90,8 @@ word_list = Box(rest, layout='grid', grid=[1, 1], border=2, align='top')
 word_box = Box(rest, layout='grid', grid=[0, 2])
 wordtitle = Text(word_list, text='Word List', grid=[0, 0])
 wordlist = Text(word_list, text='', grid=[0, 1])
-current_word_title = Text(word_list, text='Current Word:', grid=[0, 2], align='bottom')
+current_word_title = Text(word_list, text='Current Word:',
+                          grid=[0, 2], align='bottom')
 current_word = Text(word_list, text=(''.join(word)), grid=[0, 3])
 submit = PushButton(word_box, text='Submit', command=submit_word, grid=[0, 0])
 clear = PushButton(word_box, text='Clear', command=clear_word, grid=[1, 0])
